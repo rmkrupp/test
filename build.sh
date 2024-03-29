90b56884df4ed8f6cdea58ec88376ba0f5e67cab
Beka Krupp <beka.krupp@gmail.com>
2024-03-29 12:49:35 -0400

    setup test.cpp

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ -o out/testpp test.cpp && ./out/testpp
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    // do stuff only c++ can do
+    std::cout << "Hello, snrky!\n";
+}
